770e847e705bf25dc0b0783155ec58b5204c14b0
秋吉時弥 <akiyoshi.tokiya@iticket.co.jp>
2022-06-14 11:03:51 +0900

    構築完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  email              :string           default(""), not null
+#  encrypted_password :string           default(""), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 Doorkeeper::OpenidConnect.configure do
-  issuer do |resource_owner, application|
-    'issuer string'
-  end
+  # issuer do |resource_owner, application|
+  #   'issuer string'
+  # end
+  issuer 'http://localhost:3780'
 
-  signing_key <<~KEY
-    -----BEGIN RSA PRIVATE KEY-----
-    ....
-    -----END RSA PRIVATE KEY-----
-  KEY
+  # signing_key <<~KEY
+  #   -----BEGIN RSA PRIVATE KEY-----
+  #   ....
+  #   -----END RSA PRIVATE KEY-----
+  # KEY
+  signing_key File.read(Rails.root.join('jwtRS256.key'))
 
   subject_types_supported [:public]
 
   resource_owner_from_access_token do |access_token|
     # Example implementation:
-    # User.find_by(id: access_token.resource_owner_id)
+    User.find_by(id: access_token.resource_owner_id)
   end
 
   auth_time_from_resource_owner do |resource_owner|
@@ -25,9 +27,9 @@ Doorkeeper::OpenidConnect.configure do
 
   reauthenticate_resource_owner do |resource_owner, return_to|
     # Example implementation:
-    # store_location_for resource_owner, return_to
-    # sign_out resource_owner
-    # redirect_to new_user_session_url
+    store_location_for resource_owner, return_to
+    sign_out resource_owner
+    redirect_to new_user_session_url
   end
 
   # Depending on your configuration, a DoubleRenderError could be raised
@@ -44,7 +46,7 @@ Doorkeeper::OpenidConnect.configure do
 
   subject do |resource_owner, application|
     # Example implementation:
-    # resource_owner.id
+    resource_owner.id
 
     # or if you need pairwise subject identifier, implement like below:
     # Digest::SHA256.hexdigest("#{resource_owner.id}#{URI.parse(application.redirect_uri).host}#{'your_secret_salt'}")
@@ -60,13 +62,16 @@ Doorkeeper::OpenidConnect.configure do
   # expiration 600
 
   # Example claims:
-  # claims do
-  #   normal_claim :_foo_ do |resource_owner|
-  #     resource_owner.foo
-  #   end
+  claims do
+    # normal_claim :_foo_ do |resource_owner|
+    #   resource_owner.foo
+    # end
 
-  #   normal_claim :_bar_ do |resource_owner|
-  #     resource_owner.bar
-  #   end
-  # end
+    # normal_claim :_bar_ do |resource_owner|
+    #   resource_owner.bar
+    # end
+    normal_claim :email, scope: :openid do |resource_owner|
+      resource_owner.email
+    end
+  end
 end
